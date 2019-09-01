6f498a19afc7710d195684433d6e83002f4a5e4e
Ivan Berdichevsky <berdichevskyivan@gmail.com>
2019-09-01 12:28:15 -0400

    Disabled Node multiple processes

--- server/index.js
+++ server/index.js
@@ -9,92 +9,63 @@ const zoneController = require('./zoneController.js');
 const isDev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 5000;
 
-// Multi-process to utilize all CPU cores.
-if (!isDev && cluster.isMaster) {
-  console.error(`Node cluster master ${process.pid} is running`);
+const app = express();
 
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
-  });
+// Answer API requests.
+app.get('/api', function (req, res) {
+  res.set('Content-Type', 'application/json');
+  res.send('{"message":"Hello from the custom server!"}');
+});
 
-} else {
-  const app = express();
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function(request, response) {
+  response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+});
 
-  // Priority serve any static files.
-  app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+var server = app.listen(PORT, function () {
+  console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
+});
 
-  // Answer API requests.
-  app.get('/api', function (req, res) {
-    res.set('Content-Type', 'application/json');
-    res.send('{"message":"Hello from the custom server!"}');
-  });
+const io = socketio(server);
 
-  // All remaining requests return the React app, so it can handle routing.
-  app.get('*', function(request, response) {
-    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
-  });
+var loggedInUsers = {};
 
-  var server = app.listen(PORT, function () {
-    console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
-  });
+io.on('connection',function(socket){
+  socket.username = socket.handshake.query.username;
+  socket.userUniqueID = socket.handshake.query.userUniqueID;
+  if(typeof socket.username != 'undefined' && typeof socket.userUniqueID != 'undefined'){
+    if(loggedInUsers[socket.username]===socket.userUniqueID){
+      socket.emit('sessionStatus',{sessionStatus:'valid'});
+      console.log('User '+socket.username+' has connected.');
+      //GET PLAYER INFORMATION
+      socket.on('getPlayerInformation',function(){
+        db.getPlayerInfoAndEmit(socket);
+        db.getPlayerEquipmentAndEmit(socket);
+        db.getPlayerResourcesAndEmit(socket);
+        db.getPlayerItemsAndEmit(socket);
+        db.getPlayerBlueprintsAndEmit(socket);
+      });
+      //SENDER
+      socket.on('consoleMessage', function(msg){
+        console.log('Console Message sent: '+msg);
+        socket.emit('consoleMessage',msg);
+      });
 
-  const io = socketio(server);
+      //GLOBAL
+      socket.on('globalChatMessage', function(msg){
+        console.log('Global Chat Message sent: '+msg);
+        io.emit('globalChatMessage',msg);
+      });
 
-  var loggedInUsers = {};
+      socket.on('logout',function(){
+        console.log('User '+socket.username+' logged out.');
+        delete loggedInUsers[socket.username];
+        socket.emit('logout',{});
+      });
 
-  io.on('connection',function(socket){
-    socket.username = socket.handshake.query.username;
-    socket.userUniqueID = socket.handshake.query.userUniqueID;
-    if(typeof socket.username != 'undefined' && typeof socket.userUniqueID != 'undefined'){
-      if(loggedInUsers[socket.username]===socket.userUniqueID){
-        socket.emit('sessionStatus',{sessionStatus:'valid'});
-        console.log('User '+socket.username+' has connected.');
-        //GET PLAYER INFORMATION
-        socket.on('getPlayerInformation',function(){
-          db.getPlayerInfoAndEmit(socket);
-          db.getPlayerEquipmentAndEmit(socket);
-          db.getPlayerResourcesAndEmit(socket);
-          db.getPlayerItemsAndEmit(socket);
-          db.getPlayerBlueprintsAndEmit(socket);
-        });
-        //SENDER
-        socket.on('consoleMessage', function(msg){
-          console.log('Console Message sent: '+msg);
-          socket.emit('consoleMessage',msg);
-        });
-
-        //GLOBAL
-        socket.on('globalChatMessage', function(msg){
-          console.log('Global Chat Message sent: '+msg);
-          io.emit('globalChatMessage',msg);
-        });
-
-        socket.on('logout',function(){
-          console.log('User '+socket.username+' logged out.');
-          delete loggedInUsers[socket.username];
-          socket.emit('logout',{});
-        });
-
-      }else{
-        console.log('Someone has connected.');
-        socket.emit('sessionStatus',{sessionStatus:'invalid'});
-        socket.on('submitLogin', function(data){
-          let username = data.username;
-          let password = data.password;
-          db.verifyUsernameAndPasswordAndEmit(socket,username,password,loggedInUsers);
-        });
-
-        socket.on('submitSignup', function(data){
-          let username = data.username;
-          let password = data.password;
-          db.insertUsernameAndPasswordAndEmit(socket,username,password,loggedInUsers);
-        });
-      }
     }else{
       console.log('Someone has connected.');
       socket.emit('sessionStatus',{sessionStatus:'invalid'});
@@ -110,13 +81,26 @@ if (!isDev && cluster.isMaster) {
         db.insertUsernameAndPasswordAndEmit(socket,username,password,loggedInUsers);
       });
     }
-
-    socket.on('disconnect', function(){
-      console.log('User disconnected');
+  }else{
+    console.log('Someone has connected.');
+    socket.emit('sessionStatus',{sessionStatus:'invalid'});
+    socket.on('submitLogin', function(data){
+      let username = data.username;
+      let password = data.password;
+      db.verifyUsernameAndPasswordAndEmit(socket,username,password,loggedInUsers);
     });
 
+    socket.on('submitSignup', function(data){
+      let username = data.username;
+      let password = data.password;
+      db.insertUsernameAndPasswordAndEmit(socket,username,password,loggedInUsers);
+    });
+  }
+
+  socket.on('disconnect', function(){
+    console.log('User disconnected');
   });
 
-  zoneController.initializeZones(io);
+});
 
-}
+zoneController.initializeZones(io);
