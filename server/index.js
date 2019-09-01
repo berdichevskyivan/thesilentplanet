1ef4424f5f7e03930273efa5193ce4d4a5d70b3d
Ivan Berdichevsky <berdichevskyivan@gmail.com>
2019-09-01 13:38:41 -0400

    Changes rollback

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const zoneController = require('./zoneController.js');
 
 const isDev = process.env.NODE_ENV !== 'production';
 //const PORT = process.env.PORT || 5000;
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
